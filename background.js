c59d6f789d51978de680a2574dc7df8d24b58b3a
Jonathan Slate <jslate@patientslikeme.com>
2016-12-03 15:29:26 -0500

    get rid of using missing host variable

--- background.js
+++ background.js
@@ -2,8 +2,9 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     let code = 'tag=camecoho-20';
     if (details.url.indexOf(code) > 0) { return {} ; }
-    let separator = details.url.indexOf('?') > 0 ? '&' : '?'
-    return {redirectUrl: host + details.url + separator + code};
+    let separator = details.url.indexOf('?') > 0 ? '&' : '?';
+    let redirectUrl = details.url + separator + code;
+    return {redirectUrl: redirectUrl};
   },
   { urls: ["*://amazon.com/*", "*://www.amazon.com/*" ],
     types: ["main_frame"] },
